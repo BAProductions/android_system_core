c7db5b6d0e5c5cb913aa9140de5ce3c6afce1cfc
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-10-28 21:52:29 -0400

    0001-healthd-allow-home-page-button-to-wake
    [PATCH] healthd: allow home(page) button to wake

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -507,6 +507,8 @@ static void process_key(charger* charger, int code, int64_t now) {
 
 static void handle_input_state(charger* charger, int64_t now) {
     process_key(charger, KEY_POWER, now);
+	process_key(charger, KEY_HOME, now);
+	process_key(charger, KEY_HOMEPAGE, now);
 
     if (charger->next_key_check != -1 && now > charger->next_key_check)
         charger->next_key_check = -1;
